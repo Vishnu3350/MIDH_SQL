101e66ec5dd1163ecca448bf3b6d350cab138f85
Karnati Vishnu Vardhan Babu <vishnu.1615386@gmail.com>
2026-07-14 18:33:17 +0530

    DAY2_DAY3_TASK

--- SQL_TASK/SQL_DAY2_Task.sql
+++ SQL_TASK/SQL_DAY2_Task.sql
@@ -39,7 +39,7 @@ INSERT INTO Employee VALUES
 (8, 'Tan', 'tan@gmail.com', 80000, 19, 104),
 (9, 'Revi', 'revi@gmail.com', 19000, 21, 102),
 (10, 'Sai', 'sai@gmail.com', 75000, 35, 104);
-select * from Employee;
+
 
 
 # Task2 - Using the Employee and Department tables, write queries for the following:
@@ -77,7 +77,7 @@ SELECT COUNT(*) AS total_employees FROM Employee;
 SELECT MAX(employee_salary) AS maximum_salary FROM Employee;
 
 -- Minimum salary.
-SELECT MIN(employee_salary) AS minimum_salary FROM Employee;
+SELECT MIN(employee_salary)AS minimum_salary FROM Employee;
 
 -- Average salary.
 SELECT AVG(employee_salary) AS average_salary FROM Employee;
@@ -90,3 +90,89 @@ SELECT department_id, COUNT(*) AS employee_count FROM Employee GROUP BY departme
 
 -- Display employee name with department name using INNER JOIN.
 SELECT e.employee_name, d.department_name FROM Employee e INNER JOIN Department d ON e.department_id = d.department_id;
+
+
+# -------------------------------- DAY3 - TASK ----------------------------------------
+############# TASK 3 ############
+-- Part A – User Management
+select user, host from mysql.user;
+
+create user 'student1'@'%' identified by 'Student@123';
+
+show grants for 'student1'@'%';
+
+grant select on office_db. * to 'student1'@'%';
+
+-- Part B - Transactions - group of sql statements execute as a single unit.
+-- start a transaction
+START TRANSACTION;
+set autocommit = 0;
+
+UPDATE Employee
+SET employee_salary = 75000
+WHERE employee_id = 1;
+
+UPDATE Employee
+SET employee_salary = employee_salary + 10000
+WHERE employee_id = 2;
+
+UPDATE Employee
+SET employee_salary = employee_salary - 10000
+WHERE employee_id = 1;
+
+ROLLBACK;
+
+SELECT * FROM Employee;
+
+
+
+# Part C – Table Modification
+-- add
+alter table Employee add employee_phone int;
+
+-- modify
+ALTER TABLE Employee MODIFY employee_phone VARCHAR(15);
+
+-- update
+UPDATE Employee SET employee_phone = '9876543210' WHERE employee_id = 1;
+
+UPDATE Employee SET employee_phone = '9123456789' WHERE employee_id = 2;
+
+UPDATE Employee SET employee_phone = '9866280734' WHERE employee_id = 3;
+
+select * from Employee;
+
+
+-- rename
+alter table employee rename column emp_mobilenumber to mobile_number;
+
+-- TRUNCATE -> Removes all records from a table but keeps the table structure.
+
+-- DROP -> Deletes the entire table or database, including its structure and data.
+
+-- DELETE -> Deletes rows from a table. The table structure remains, and you can delete specific rows using a WHERE clause.
+
+########## TASK-4 ###########
+-- Create one subquery to display the employee with the highest salary.
+SELECT *
+FROM Employee
+WHERE employee_salary = (
+    SELECT MAX(employee_salary)
+    FROM Employee
+);
+
+-- Create one stored procedure to display all employee records.
+DELIMITER $$
+
+CREATE PROCEDURE GetAllEmployees()
+BEGIN
+    SELECT * FROM Employee;
+END $$
+
+DELIMITER ;
+CALL GetAllEmployees();
+
+-- Create one index on the emp_name column.
+CREATE INDEX idx_employee_name
+ON Employee(employee_name);
+SHOW INDEX FROM Employee;
